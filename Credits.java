4b3ee79bd24a9aa4e6f62c2973ac079b23946d49
Joey <jmullercuber@gmail.com>
2015-07-11 13:36:36 -0600

    Correcting the size of the world

--- Credits.java
+++ Credits.java
@@ -16,6 +16,6 @@ public class Credits extends World
     public Credits()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1008, 630, 1); 
     }
 }
--- back.java
+++ back.java
@@ -17,7 +17,7 @@ public class back extends World
     public back()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1008, 800, 1);
+        super(1008, 630, 1);
         prepare();
     }
     
